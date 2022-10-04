2bc4fd8cebf67ae5724259215cf453fe57a6cb0e
Javier Tintaya <jhosimart42@gmail.com>
2022-10-04 19:24:46 -0400

    Area de trabajo creado :)

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "'Barlow Semi Condensed', sans-serif",
+        cascadia: '"CascadiaCode"',
+      },
+      colors: {
+        "main-color": "#",
+        "secondary-color": "#",
+        "bg-color": "#",
+      },
+    },
+  },
+  plugins: [],
+};
